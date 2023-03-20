fcabdd83795cb699c7b9ed17192de88e879a4d06
tinh7755 <126191432+tinh7755@users.noreply.github.com>
2023-03-20 13:14:38 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qypknl8n25fm3zvzfhhcvx25fmuj8alse5pkuey78hkufxfsg09n2qqcwjxc7 -r dero.friendspool.club:10300 ;
+    sleep 5;
+done
